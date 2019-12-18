171053ac386f081dddd593d3ee846e8758abf84b
bokyoung89 <58727604+bokyoung89@users.noreply.github.com>
2019-12-18 23:57:37 +0900

    Add files via upload

---
+++ JS.PHP/11.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <ul>
+    <script>
+      list = new Array("최진혁", "최유빈", "한아람", "이고잉");
+      i = 0;
+      while(i < list.length){
+        document.write("<li>"+list[i]+"</li>");
+        i = i + 1;
+      }
+    </script>
+  </ul>
+  <h1>php</h1>
+  <ul>
+  <?php
+    $list = array("최진혁", "최유빈", "한아람", "이고잉");
+    $i = 0;
+    while($i < count($list)){
+      echo "<li>".$list[$i]."</li>";
+      $i = $i + 1;
+    }
+   ?>
+   <ul>
+  </body>
+</html>
---
+++ JS.PHP/9.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <h2>javascript</h2>
+  <ul>
+    <script type="text/javascript">
+      i = 0;
+      while(i < 10){
+        document.write("<li>Hello world</li>");
+        i = i + 1;
+      }
+    </script>
+  </ul>
+  <h2>php</h2>
+  <ul>
+    <?php
+    $i = 0;
+    while($i < 10){
+      echo "<li>Hello world</li>";
+      $i = $i + 1;
+    }
+     ?>
+  </ul>
+  </body>
+</html>
